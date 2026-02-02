390a7ca11ddccf776b6324f9d162d68337ffd207
Abhishikta24Pal <pal24abhi@gmail.com>
2026-02-02 13:25:26 +0530

    Case-sensitive Fix

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { auth, googleProvider } from "../firebase";
+import { auth, googleProvider } from "../firebase.js";
 import {
   onAuthStateChanged,
   signInWithEmailAndPassword,
